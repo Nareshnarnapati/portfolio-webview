9e036658c00c2b132ecfc8e4ffd63fea031d4cbc
nareshnarnapati <nareshnarnapati@gmail.com>
2024-04-02 18:07:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Skills from './Skills';
-
 import Projects from './Projects';
 import Contact from './Contact';
+import NotFound from './NotFound';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/projects" element={<Projects/>}/>
-        <Route path="/contact" element={<Contact/>}/>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/skills" element={<Skills />} />
+        <Route exact path="/projects" element={<Projects />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
